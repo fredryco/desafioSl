90f2cd80854e35f9cdb522d0ce31d4ebff0ba9ec
Fred <Fred@DESKTOP-D11NP9D>
2018-09-12 20:43:30 -0300

    commit inicial da estrutura do projeto

---
+++ desafioSolutis/src/main/java/com/desafioSolutis/desafioSolutis/DesafioSolutisApplication.java
@@ -0,0 +1,12 @@
+package com.desafioSolutis.desafioSolutis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSolutisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSolutisApplication.class, args);
+	}
+}
---
+++ desafioSolutis/src/main/java/com/desafioSolutis/desafioSolutis/PalavraEntity.java
@@ -0,0 +1,10 @@
+package com.desafioSolutis.desafioSolutis;
+
+import java.util.List;
+
+public class PalavraEntity {
+	
+	private String palavra;
+	private List<String> letra;
+
+}
